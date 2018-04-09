aa2a5bb87a536edbffeb61555b1ee77a713abd54
Andrew Voorhis <avoorhis@mbl.edu>
2018-04-09 14:06:07 -0400

    clarify syncrony

---
+++ metadata_files_synchrony.py
@@ -0,0 +1,383 @@
+#!/usr/bin/env python
+
+"""
+  create_counts_lookup.py
+
+
+"""
+
+import sys,os,io
+import argparse
+try:
+    import pymysql as mysql
+except ImportError:
+    import MySQLdb as mysql
+except:
+    raise
+import json
+import shutil
+import datetime
+import socket
+
+today     = str(datetime.date.today())
+
+
+"""
+
+"""
+
+
+parser = argparse.ArgumentParser(description="")
+
+
+def convert_keys_to_string(dictionary):
+    """Recursively converts dictionary keys to strings."""
+    if not isinstance(dictionary, dict):
+        return dictionary
+    return dict((str(k), convert_keys_to_string(v))
+        for k, v in dictionary.items())
+
+def go_list(args):
+
+    #
+    file_dids = []
+
+    metadata_lookup = convert_keys_to_string(read_original_metadata())
+    (projects_by_did, project_id_lookup, project_lookup) = get_project_lookup(args) # from database
+    #print(project_lookup)
+    required_metadata_fields = get_required_metadata_fields(args)
+    #print('file_dids')
+    #print(metadata_lookup)  ## <-- lookup by did
+
+    #metadata_dids = metadata_lookup.keys()
+    #
+    #print file_dids
+    #print len(file_dids)
+    #q =  "SELECT dataset_id,dataset.project_id,project from project"
+    #q += " JOIN dataset using(project_id) order by project"
+    failed_projects = []
+    no_req_data_found = 0
+    no_file_found = {}          # 2only for metadata BULK FILE: if did NOT found
+    mismatch_data = {}          # 1metadata mismatch between DATABASE and metadata BULK FILE
+    cust_rowcount_data = {}     # 4dataset count unequal between DATABASE custom metadata table and DATABASE datasets table
+    other_problem = {}
+    did_file_problem = {}       # 5did FILES: if taxcounts ={} or empty file present
+    no_req_metadata = {}        # 3if no required metadata found in DATABASE
+    num_cust_rows = 0 
+    if args.single_pid:
+        temp_id_lookup = {}
+        temp_id_lookup[args.single_pid] = project_id_lookup[args.single_pid]
+        project_id_lookup = temp_id_lookup
+        print('Searching Single PID:',args.single_pid)
+        print(project_id_lookup)
+        ds_count = len(project_id_lookup[args.single_pid]) 
+        print("ds_count from DB:",ds_count)
+        
+    for pid in project_id_lookup:
+        # go project by project
+        sql_dids =  "','".join(project_id_lookup[pid])
+        ds_count = len(project_id_lookup[pid]) 
+        q = "SELECT dataset_id, "+ ', '.join(required_metadata_fields) +" from required_metadata_info WHERE dataset_id in ('%s')" % (sql_dids)
+        if args.verbose:
+            print(q)
+        clean_project = True
+        num = 0
+        cur.execute(q)
+        numrows = cur.rowcount
+        if numrows == 0:
+            no_req_data_found += 1
+            #print(str(no_req_data_found)+') No Required metadata for project: '+str(project_lookup[pid])+' ('+str(pid)+')')
+            if pid not in no_req_metadata:
+                    no_req_metadata[pid] = project_lookup[pid]
+            continue
+
+        for row in cur.fetchall():
+            did = str(row[0])
+            #print 'did',did
+            #if did == '3938':
+            if did not in metadata_lookup:
+                if pid not in no_file_found:
+                    no_file_found[pid] = project_lookup[pid]
+                clean_project = False
+            else:
+                for i,item in enumerate(required_metadata_fields):
+                    #print(item, row[i+1])
+                    if item in metadata_lookup[did]:
+                        db_val = str(row[i+1])
+                        if str(metadata_lookup[did][item]) != db_val :
+                            if args.verbose:
+                                print('pid:'+str(pid) +' -- '+project_lookup[pid]+' -- did:' +did+'  no match-1 for `'+item+'`:',metadata_lookup[did][item],' - ',db_val)
+                            if pid not in mismatch_data:
+                                mismatch_data[pid] = project_lookup[pid]
+                            clean_project = False
+                    else:
+                         if args.verbose:
+                            print( 'pid:'+str(pid) +' -- '+'--'+project_lookup[pid]+' -- did:' +did+' -- `'+item+'` item not found in req metadata file-1\n')
+                         if pid not in other_problem:
+                            other_problem[pid] = project_lookup[pid]
+                         clean_project = False
+            try:
+                did_file = os.path.join(args.json_file_path,NODE_DATABASE+'--datasets_silva119',str(did)+'.json')
+                fp = open(did_file)
+                file_data = json.load(fp)
+                if not file_data or not file_data['taxcounts']:
+                    did_file_problem[pid] = project_lookup[pid]
+                fp.close()
+            except:
+                did_file_problem[pid] = project_lookup[pid]
+                if args.verbose:
+                    print( 'pid:'+str(pid) +' -- '+'--'+project_lookup[pid]+' -- did:' +did+' -- Could not open did file-1\n')
+        
+        custom_metadata_file = 'custom_metadata_'+str(pid)
+        
+        q1 = "SHOW fields from "+custom_metadata_file
+        q2 = "SELECT * from "+custom_metadata_file
+
+        fields = []
+        try:
+            cur.execute(q1)
+            rows = cur.fetchall()
+            for row in rows:
+                #print row
+                field = str(row[0])
+                if field !=  custom_metadata_file+'_id' and field != 'dataset_id' and field != 'project_id':
+                    fields.append(field) # starts with idx 2
+            #print 'fields',fields
+            if args.verbose:
+                print(q2)
+            # query2 q2 = "SELECT * from "+custom_metadata_file
+            cur.execute(q2)
+            num_cust_rows = cur.rowcount
+            if num_cust_rows != ds_count:
+                cust_rowcount_data[pid] = project_lookup[pid]
+            
+            rows = cur.fetchall()
+            for row in rows:
+                did = str(row[1]) # first is custom_metadata_<pid>_id, second is did
+                #print
+                #print 'row',row
+                for i,item in enumerate(fields):
+
+                    #print('pid:',pid,'did:',did, 'item:',item, metadata_lookup[did], project_lookup[pid])
+                    if item in metadata_lookup[did]:
+                        #print(item,i,'row',row)
+                        db_val = str(row[i+2])
+
+                        if str(metadata_lookup[did][item]) != db_val:
+                            if args.verbose:
+                                print('pid:'+str(pid) +' -- '+project_lookup[pid]+' -- did:' +did+'  no match-2 for `'+item+'`:',metadata_lookup[did][item],'!=',db_val)
+                            if pid not in mismatch_data:
+                                mismatch_data[pid] = project_lookup[pid]
+                            clean_project = False
+                    else:
+                        if args.verbose:
+                            print( 'pid:'+str(pid) +' -- '+project_lookup[pid]+' -- did:' +did+' -- `'+item+'` item not found in cust metadata file-2\n')
+                        if pid not in other_problem:
+                            other_problem[pid] = project_lookup[pid]
+                        clean_project = False
+        except:
+            #print "FYI: No Custom Metadata:",custom_metadata_file,"is missing"
+            pass
+        #sys.exit()
+        #if not clean_project:
+        #      failed_projects.append('pid:'+str(pid)+' -- '+project_lookup[pid])
+    
+    print()
+    print('*'*60)
+    print('Failed projects:')
+    
+    
+    print()
+    print('\tMETADATA MIS-MATCHES BETWEEN BULK FILE AND DBASE (Assumes same for did file)(re-build should work):')
+    if not len(mismatch_data):
+        print('\t **Clean**')
+    else:
+        for pid in mismatch_data:
+            print( '\t pid:',pid,' -- ',mismatch_data[pid])
+        print ('\t PID List:',','.join([str(n) for n in mismatch_data.keys()]))
+    print()
+    print('\tNO DID FOUND IN METADATA BULK FILE (Assumes no did file found either) (re-build should work):')
+    if not len(no_file_found):
+        print('\t **Clean**')
+    else:
+        for pid in no_file_found:
+            print('\t pid:',pid,' -- ',no_file_found[pid])
+        print('\t PID List:',','.join([str(n) for n in no_file_found.keys()]))
+    print()
+    print('\tNO REQUIRED METADATA FOUND IN DATABASE (re-install project or add by hand -- re-build won\'t help):')
+    if not len(no_req_metadata):
+        print('\t **Clean**')
+    else:
+        for pid in no_req_metadata:
+            print('\t pid:',pid,' -- ',no_req_metadata[pid])
+        print('\t PID List:',','.join([str(n) for n in no_req_metadata.keys()]))
+    print()
+    print('\tDATABASE: Dataset count is different between `dataset` and `custom_metadata_xxx` tables (re-build won\'t help):')
+    if not len(cust_rowcount_data):
+        print('\t **Clean**')
+    else:
+        for pid in cust_rowcount_data:
+            print('\t pid:',pid,' -- ',cust_rowcount_data[pid])
+        print  ('\t PID List:',','.join([str(n) for n in cust_rowcount_data.keys()]))
+    print()
+    print('\tDID FILES: zero-length file or taxcounts={}:')
+    if not len(did_file_problem):
+        print('\t **Clean**')
+    else:
+        for pid in did_file_problem:
+            print('\t pid:',pid,' -- ',did_file_problem[pid])
+        print  ('\t PID List:',','.join([str(n) for n in did_file_problem.keys()]))
+    print()
+    print('\tOTHER (rare -- Possible DID mis-match or case difference for metadata -- re-build may or may not help):')
+    if not len(other_problem):
+        print('\t **Clean**')
+    else:
+        for pid in other_problem:
+            print('\t pid:',pid,' -- ',other_problem[pid])
+        print  ('\t PID List:',','.join([str(n) for n in other_problem.keys()]))
+    print()
+    print("Number of files that should be rebuilt:",len(other_problem)+len(mismatch_data)+len(no_file_found))
+    print('*'*60)
+
+def read_original_metadata():
+    file_path = os.path.join(args.json_file_path,NODE_DATABASE+'--metadata.json')
+    try:
+        with open(file_path) as data_file:
+            data = json.load(data_file)
+    except:
+        print("could not read json file",file_path,'-Exiting')
+        sys.exit(1)
+    return data
+
+
+def get_required_metadata_fields(args):
+    q =  "SHOW fields from required_metadata_info"
+    cur.execute(q)
+    md_fields = []
+    fields_not_wanted = ['required_metadata_id','dataset_id','created_at','updated_at']
+    for row in cur.fetchall():
+        if row[0] not in fields_not_wanted:
+            md_fields.append(row[0])
+    return md_fields
+
+def get_project_lookup(args):
+    q =  "SELECT dataset_id, dataset.project_id, project from project"
+    q += " JOIN dataset using(project_id) where project not like '%_Sgun' order by project"
+
+    num = 0
+    cur.execute(q)
+    #print 'List of projects in: '+in_file
+    projects_by_did = {}
+    project_id_lookup = {}
+    project_lookup = {}
+
+    for row in cur.fetchall():
+        did = str(row[0])
+        pid = str(row[1])
+        pj  = row[2]
+        project_lookup[pid]  = pj
+        projects_by_did[did] = pj
+        if pid in project_id_lookup:
+            project_id_lookup[pid].append(str(did))
+        else:
+            project_id_lookup[pid] = [str(did)]
+
+    return (projects_by_did, project_id_lookup, project_lookup)
+#
+#
+#
+if __name__ == '__main__':
+
+    myusage = """
+    
+    metadata_files_synchrony.py
+    
+        -host/--host        vampsdb, vampsdev    dbhost:  [Default: localhost]
+        
+        -v/--verbose    lots of talk          
+        
+        -pid/--pid       will check a single pid for file consistancy
+        
+        -json_file_path/--json_file_path
+    
+    """
+
+    parser.add_argument("-json_file_path", "--json_file_path",
+                required=False,  action='store', dest = "json_file_path",  default='json',
+                help="Not usually needed if -host is accurate")
+    parser.add_argument("-host", "--host",
+                required=False,  action='store', dest = "dbhost",  default='localhost',
+                help="choices=['vampsdb','vampsdev','localhost']")
+    parser.add_argument("-v", "--verbose",
+                required=False,  action='store_true',  dest = "verbose",  default=False,
+                help="")
+    parser.add_argument("-pid", "--pid",
+                required=False,  action='store',  dest = "single_pid",  default='',
+                help="Will check a single pid for consistancy")
+    if len(sys.argv[1:]) == 0:
+        print(myusage)
+        sys.exit()
+    args = parser.parse_args()
+
+
+    if args.dbhost == 'vamps' or args.dbhost == 'vampsdb':
+        args.json_file_path = '/groups/vampsweb/vamps_node_data/json'
+        dbhost = 'vampsdb'
+        args.NODE_DATABASE = 'vamps2'
+
+    elif args.dbhost == 'vampsdev':
+        args.json_file_path = '/groups/vampsweb/vampsdev_node_data/json'
+        args.NODE_DATABASE = 'vamps2'
+        dbhost = 'bpcweb7'
+    elif args.dbhost == 'localhost' and (socket.gethostname() == 'Annas-MacBook.local' or socket.gethostname() == 'Annas-MacBook-new.local'):
+        args.NODE_DATABASE = 'vamps2'
+        dbhost = 'localhost'
+    else:
+        args.NODE_DATABASE = 'vamps_development'
+        dbhost = 'localhost'
+    args.units = 'silva119'
+    if args.units == 'silva119':
+        args.files_prefix   = os.path.join(args.json_file_path, args.NODE_DATABASE+"--datasets_silva119")
+    elif args.units == 'rdp2.6':
+         args.files_prefix   = os.path.join(args.json_file_path, args.NODE_DATABASE+"--datasets_rdp2.6")
+    else:
+        sys.exit('UNITS ERROR: '+args.units)
+
+    if os.path.exists(args.json_file_path):
+        print('Validated: json file path')
+    else:
+        print(myusage)
+        print("Could not find json directory: '",args.json_file_path,"'-Exiting")
+        sys.exit(-1)
+    print("\nARGS: dbhost  =",dbhost)
+    print("ARGS: json_dir=",args.json_file_path)
+
+    db = mysql.connect(host=dbhost, # your host, usually localhost
+                             read_default_file="~/.my.cnf_node"  )
+    cur = db.cursor()
+    if args.NODE_DATABASE:
+        NODE_DATABASE = args.NODE_DATABASE
+    else:
+        cur.execute("SHOW databases like 'vamps%'")
+        dbs = []
+        print(myusage)
+        db_str = ''
+        for i, row in enumerate(cur.fetchall()):
+            dbs.append(row[0])
+            db_str += str(i)+'-'+row[0]+';  '
+        print(db_str)
+        db_no = input("\nchoose database number: ")
+        if int(db_no) < len(dbs):
+            NODE_DATABASE = dbs[db_no]
+        else:
+            sys.exit("unrecognized number -- Exiting")
+
+    print()
+    cur.execute("USE "+NODE_DATABASE)
+
+    #out_file = "tax_counts--"+NODE_DATABASE+".json"
+    #in_file  = "../json/tax_counts--"+NODE_DATABASE+".json"
+    
+    print('DATABASE:',NODE_DATABASE)
+
+    go_list(args)
